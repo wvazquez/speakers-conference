eedfa67cb8311e1e37fc762984f8541b3b1e1079
wvazquez <vazquezwilmer@gmail.com>
2019-03-11 20:41:02 -0400

    Sets up an error page handler.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const createErrors = require('http-errors');
 const routes = require('./routes');
 const path = require('path');
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, './views'));
 
@@ -15,6 +17,17 @@ app.get('/favicon.ico', (req,res,next)=>{
     return res.sendStatus(204);
 });
 app.use('/', routes());
+app.use((req,res,next) => {
+    return next(createErrors(404, "File Not Found"));
+});
+app.use((err,req,res,next) => {
+    res.locals.message = err.message;
+    const status = err.status || 500;
+    res.locals.status = status;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(status);
+    return res.render('404');
+});
 
 app.listen(3000);
 
