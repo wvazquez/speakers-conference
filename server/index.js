9705f05f63b142e44d6e8596756c3e57fc39a595
wvazquez <vazquezwilmer@gmail.com>
2019-03-12 01:09:48 -0400

    Adds dynamic message and status code to error page.

--- server/index.js
+++ server/index.js
@@ -18,15 +18,15 @@ app.get('/favicon.ico', (req,res,next)=>{
 });
 app.use('/', routes());
 app.use((req,res,next) => {
-    return next(createErrors(404, "File Not Found"));
+    return next(createErrors(404, "Page Not Found"));
 });
 app.use((err,req,res,next) => {
-    res.locals.message = err.message;
     const status = err.status || 500;
     res.locals.status = status;
+    res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.status(status);
-    return res.render('404');
+    return res.render('error');
 });
 
 app.listen(3000);
