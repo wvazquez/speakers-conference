cfc757db44e5a858ff36c0084f7bc1807d505326
wilmer vazquez <vazquezwilmer@gmail.com>
2019-04-19 08:38:18 -0400

    Changes the speakers page to aa .pug file.

--- server/routes/speakers/index.js
+++ server/routes/speakers/index.js
@@ -3,9 +3,7 @@ const router = express.Router();
 
 module.exports = () => {
     router.get('/', (req,res,next)=>{
-        return res.render('speakers/speakers',{
-            layout: 'default'
-        });
+        return res.render('speakers');
     });
     router.get('/:name', (req,res,next)=>{
         return res.send(`Speaker ${req.params.name}`);
