e9ddbaa09c1f2e2444dddf30632e6f9655436774
wvazquez <vazquezwilmer@gmail.com>
2019-04-27 17:55:20 -0400

    Updates to express 4 and moves startup scripts to www in bin folder.

--- server/index.js
+++ server/index.js
@@ -3,74 +3,73 @@ const createErrors = require('http-errors');
 const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-const configs = require('./config');
 const SpeakerService = require('./services/SpeakerService');
 const FeedbackService = require('./services/FeedbackService');
 
-const app = express();
 
-const config = configs[app.get('env')];
-
-const speakerService = new SpeakerService(config.data.speakers);
-const feedbackService = new FeedbackService(config.data.feedback);
+module.exports = (config) =>{
+    const app = express();
 
 
-// static assets in public folder
-app.use(express.static('public'));
-
-// view engine setup
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, './views'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// formats page source to have whitespace in development mode
-if(app.get('env') === 'development'){
-    app.locals.pretty = true;
-}
+    const speakerService = new SpeakerService(config.data.speakers);
+    const feedbackService = new FeedbackService(config.data.feedback);
 
 
-app.locals.title = config.sitename;
-console.log(app.locals.title);
+    // static assets in public folder
+    app.use(express.static('public'));
 
-// Temp solution for no favicon found
-app.get('/favicon.ico', (req,res,next)=>{
-    return res.sendStatus(204);
-});
+    // view engine setup
+    app.set('view engine', 'pug');
+    app.set('views', path.join(__dirname, './views'));
 
-app.use(async (req,res,next) => {
-    try {
-        const names = await speakerService.getNames();
-        res.locals.speakerNames = names;
-        return next();
-    } catch (err) {
-        return next(err)
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    // formats page source to have whitespace in development mode
+    if(app.get('env') === 'development'){
+        app.locals.pretty = true;
     }
-});
 
-// modular routing
-app.use('/', routes({
-    speakerService,
-    feedbackService,
-}));
 
-// If we reach this point, we know no other routes have been found.
-// This is a normal route that is matched if no other routes have matched. 
-// Our route will match and send an error to next() and pass to error handler
-app.use((req,res,next) => {
-    return next(createErrors(404, "Page Not Found"));
-});
+    app.locals.title = config.sitename;
+    console.log(app.locals.title);
 
-// error handler function that will handle any error passed through next()
-app.use((err,req,res,next) => {
-    const status = err.status || 500;
-    res.locals.status = status;
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(status);
-    return res.render('error');
-});
+    // Temp solution for no favicon found
+    app.get('/favicon.ico', (req,res,next)=>{
+        return res.sendStatus(204);
+    });
 
-app.listen(3000);
+    app.use(async (req,res,next) => {
+        try {
+            const names = await speakerService.getNames();
+            res.locals.speakerNames = names;
+            return next();
+        } catch (err) {
+            return next(err)
+        }
+    });
 
-module.export = app;
+    // modular routing
+    app.use('/', routes({
+        speakerService,
+        feedbackService,
+    }));
+
+    // If we reach this point, we know no other routes have been found.
+    // This is a normal route that is matched if no other routes have matched. 
+    // Our route will match and send an error to next() and pass to error handler
+    app.use((req,res,next) => {
+        return next(createErrors(404, "Page Not Found"));
+    });
+
+    // error handler function that will handle any error passed through next()
+    app.use((err,req,res,next) => {
+        const status = err.status || 500;
+        res.locals.status = status;
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+        res.status(status);
+        return res.render('error');
+    });
+
+    return app;
+}
