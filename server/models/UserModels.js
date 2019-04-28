a230cbf0ceeddcf79810741414b871680b4fbb1e
wvazquez <vazquezwilmer@gmail.com>
2019-04-28 10:49:36 -0400

    Encypts password information.

--- server/models/UserModels.js
+++ server/models/UserModels.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const emailValidator = require('email-validator');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 12;
 
 const UserSchema = mongoose.Schema({
     username: {
@@ -31,5 +34,23 @@ const UserSchema = mongoose.Schema({
 },{
     timestamps: true
 });
+
+UserSchema.pre('save', async function preSave(next){
+    const user  = this;
+    if(!user.isModified('password')){
+        return next();
+    }
+    try {
+        const hash = await bcrypt.hash(user.password, SALT_ROUNDS)
+        user.password = hash;
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+UserSchema.methods.comparePassword = async function comparePassword(candidate){
+    return bcrypt.compare(candidate, this.password);
+;}
 
 module.exports = mongoose.model("User", UserSchema);
