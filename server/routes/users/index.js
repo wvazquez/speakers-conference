5e960f980fa1c6f7f877ec999c83adb717e062c3
wvazquez <vazquezwilmer@gmail.com>
2019-04-28 12:27:05 -0400

    Creates the route for the post request to create a new user.

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const UserModel = require('../../models/UserModels');
 const router = express.Router();
 
 module.exports = (params) => {
@@ -7,6 +7,21 @@ module.exports = (params) => {
 
   router.get('/registration', (req, res) =>
     res.render('users/registration', { success: req.query.success }));
+
+  router.post('/registration', async (req,res,next) =>{
+    try {
+      const user = new UserModel({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+      });
+      const savedUser = user.save();
+      if(savedUser) return res.redirect('/users/registration?success=true');
+      return next(new Error("Failed to save user."))
+    } catch (err) {
+      return next(err);
+    }git s
+  });  
 
   router.get('/account', (req, res) =>
     res.render('users/account', { user: req.user }));
